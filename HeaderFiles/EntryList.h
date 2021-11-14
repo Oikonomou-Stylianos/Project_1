9c0ffd0bb53f354a43c75bc403c89deb689ef6c5
anastasist <triant98@hotmail.com>
2021-11-14 19:08:34 +0200

    MInor changes

--- HeaderFiles/EntryList.h
+++ HeaderFiles/EntryList.h
@@ -18,16 +18,18 @@
 
 #include "common_types.h"
 
+//Generic List functions, called by entry list functions
 List *create_list(void);
 ErrorCode destroy_node(listnode *, ListType);
 ErrorCode destroy_list(List *, ListType);
 List *insert_list(List *, void *);
 
+//Entry List and index functions
 ErrorCode create_entry_list(List **);
 Entry *create_entry(const char *, void *);
 ErrorCode destroy_entry(Entry *);
 unsigned int get_number_entries(const List *);
-ErrorCode add_entry(List *, const Entry *); //Prefer to use create_entry as second argument
+ErrorCode add_entry(List *, const Entry *); //Prefer to use create_entry/copy_entry as second argument
 Entry *get_first(const List *);
 Entry *get_next(const listnode *);
 ErrorCode destroy_entry_list(List *);
