3970b8877ffd2a7e61b450030ee6eadf13963910
Stylianos Oikonomou <steoik@hotmail.com>
2021-12-07 00:23:27 +0200

    added libcore.c

---
+++ updated/libcore.c
@@ -0,0 +1,26 @@
+///////////////////////////////////
+// University: UoA DIT
+// Course : [K23a] Software Development for Computer Systems
+// Semester : Fall 2021-2022
+// Professor : Yannis Ioannidis 
+// --------------------------------
+// Students: Stylianos Oikonomou , Anastasios Triantafyllou
+// ID : 1115201500243 , 1115201600173 
+// --------------------------------
+// Project : Assignment_1
+// File : WordList.c
+///////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+
+#include "LinkedList.h"
+#include "BKTree.h"
+#include "HashTable.h"
+#include "constructors.h"
+#include "common_types.h"
+#include "core.h"
+
